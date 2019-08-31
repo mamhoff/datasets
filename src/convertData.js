9e6852f83c3930556f85a4b234dd4f8c6a048aba
Martin Meyerhoff <mamhoff@gmail.com>
2019-08-31 23:34:42 +0200

    Add function to parse data.csv file
    This adds a second function to the data.csv file that parses the
    data.csv file for a given directory using the item definitions from the
    same directory.

--- src/convertData.js
+++ src/convertData.js
@@ -22,3 +22,22 @@ export async function getItemDefinitions(path) {
   })
   return returnValue;
 }
+
+export async function getData(path) {
+  const itemDefinitions = await getItemDefinitions(path)
+  const csvData = fs.readFileSync(path + "/data.csv", "utf8");
+  const parsedCsv = await neatCsv(csvData, {
+    mapValues: ({ header, index, value }) => {
+      if (value === "") {
+        return null;
+      } else if (header.startsWith('is')) {
+        return value == "true"
+      } else if (itemDefinitions[header].type === "DECIMAL") {
+        return parseFloat(value)
+      } else {
+        return value.trim()
+      }
+    }
+  })
+  return parsedCsv;
+}
